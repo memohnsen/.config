07c74178bb30839503cf85df9c6f341e916de383
Maddisen <memohnsen@gmail.com>
2026-07-01 17:45:07 -0400

    zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,14 +1,4 @@
-# Better ls
-alias ls='eza --icons'
-
-# Detailed listing
-alias ll='eza -lh --icons --git'
-
-# Detailed listing including hidden files
-alias la='eza -lah --icons --git'
-
-# Tree view
-alias tree='eza --tree --icons'
+alias ls='eza --tree --icons -lah'
 
 # Reuse ls completions for eza (avoids defining a separate completion function)
 compdef eza=ls
@@ -45,20 +35,5 @@ lf() { # zsh follow lf navigation
 # =========================================================
 
 alias vim='nvim'
-alias emacs='/opt/homebrew/bin/emacs -nw'
 alias nivm='/opt/homebrew/bin/nvim'
 alias nvin='/opt/homebrew/bin/nvim'
-
-# =========================================================
-# Git
-# =========================================================
-
-alias glog='PAGER="less -F -X" git log'                              # -F quit if one screen, -X no clear on exit
-alias gadog='PAGER="less -F -X" git log --all --decorate --oneline --graph'
-alias dotfiles='git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
-
-# =========================================================
-# Video
-# =========================================================
-
-alias stream='mpv av://v4l2:/dev/video4 --fullscreen --demuxer-lavf-o=input_format=mjpeg,framerate=30 --profile=low-latency --untimed'
