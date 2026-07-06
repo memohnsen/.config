496dc4a9810502b887beff467c35cd15965ff6f4
Maddisen <memohnsen@gmail.com>
2026-07-06 11:44:29 -0400

    remvoe plugins

--- nvim/lua/custom/plugins/orgmode.lua
+++ nvim/lua/custom/plugins/orgmode.lua
@@ -1,8 +1,15 @@
--- Personal orgmode fork (~/dev/orgmode, github.com/memohnsen/orgmode).
+-- Personal orgmode fork (github.com/memohnsen/orgmode).
 -- All workflow customizations (ux, notes, theme, bullets, snippets,
 -- super-agenda) live in the fork's lua/orgmode/pack/ modules and activate
--- from setup(). Loaded from the local checkout so edits are live.
-vim.opt.runtimepath:prepend(vim.fn.expand '~/dev/orgmode')
+-- from setup().
+vim.pack.add {
+  { src = 'https://github.com/memohnsen/orgmode' },
+}
+
+-- To develop against the local checkout instead of the GitHub repo, comment out
+-- the pack entry above and uncomment these two lines:
+-- vim.opt.runtimepath:prepend(vim.fn.expand '~/dev/orgmode')
+-- vim.cmd 'runtime plugin/orgmode.lua'
 
 require('orgmode').setup {
   pack = {
